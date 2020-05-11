2eff43c77d1703d9d6e9d6fdee4c8b7de249fd95
Quint Guvernator <quint@guvernator.net>
2020-05-11 23:46:54 +0200

    press reset to switch between work and break

--- src/cybair.cpp
+++ src/cybair.cpp
@@ -1,6 +1,8 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 #include <color.h>
+#include <FS.h>
+#include <SPIFFS.h>
 
 #define LED_PIN (32)
 #define COLS (8)
@@ -14,21 +16,63 @@
 #define POMO_ALERT_FLASHES (4)
 #define POMO_ALERT_BETWEEN_FLASHES_MS (400)
 #define POMO_ALERT_REPETITIONS (3)
+#define BREAKFILE_PATH "/start_with_break"
 
 Adafruit_NeoPixel strip(COLS * ROWS, LED_PIN);
 
-bool working;
 bool was_working;
+bool start_with_break;
+int flash_color[3];
 int flashes;
 int flash_groups;
 
 void setup(void)
 {
-	working = true;
+	start_with_break = false;
 	was_working = true;
 	flashes = 0;
 	flash_groups = 0;
+	flash_color[0] = 0xff;
+	flash_color[1] = flash_color[2] = 0;
 	strip.begin();
+
+	bool needs_flash = !SPIFFS.begin(false);
+	if (needs_flash) {
+
+		// solid purple to show formatting
+		strip.clear();
+		for (int i = 0; i < ROWS * COLS; i++) {
+			strip.setPixelColor(i, 0x10, 0, 0x20);
+		}
+		strip.show();
+
+		// flash the filesystem
+		bool ok = SPIFFS.begin(true);
+
+		if (ok) {
+			flash_groups = 1;
+			flash_color[0] = 0;
+			flash_color[1] = 0x10;
+			flash_color[2] = 0;
+		} else {
+			flash_groups = -1;
+			flash_color[0] = 0x10;
+			flash_color[1] = 0;
+			flash_color[2] = 0;
+			return;
+		}
+
+    }
+
+	if (SPIFFS.exists(BREAKFILE_PATH)) {
+		SPIFFS.remove(BREAKFILE_PATH);
+		start_with_break = true;
+
+	} else {
+		File f = SPIFFS.open(BREAKFILE_PATH, FILE_WRITE);
+		f.print("\n");
+		f.close();
+	}
 }
 
 void loop(void)
@@ -39,7 +83,7 @@ void loop(void)
 		/* all on */
 		strip.clear();
 		for (int i = 0; i < ROWS * COLS; i++)
-			strip.setPixelColor(i, working ? 60 : 0, 20, working ? 0 : 60);
+			strip.setPixelColor(i, flash_color[0], flash_color[1], flash_color[2]);
 		strip.show();
 		delay(POMO_ALERT_FLASH_ON_MS);
 
@@ -55,20 +99,30 @@ void loop(void)
 	}
 
 	if (flash_groups) {
-		flash_groups--;
+
+		if (flash_groups > 0)
+			flash_groups--;
 
 		flashes = POMO_ALERT_FLASHES + 1;
 
 		return;
 	}
 
-	uint32_t t = millis() % POMO_CYCLE_MS;
+	uint32_t t = millis();
+	if (start_with_break) {
+		t += POMO_WORK_MS;
+		was_working = false;
+	}
+	t %= POMO_CYCLE_MS;
 
-	working = t < POMO_WORK_MS;
+	const bool working = t < POMO_WORK_MS;
 	if (was_working != working) {
 		was_working = working;
 
 		flash_groups = POMO_ALERT_REPETITIONS;
+		flash_color[0] = working ? 60 : 0;
+		flash_color[1] = 20;
+		flash_color[2] = working ? 0 : 60;
 
 		return;
 	}
